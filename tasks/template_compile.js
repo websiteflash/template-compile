7867542d2c1885bd9cd7a7bb3311e5983fd531d5
jesse <delsen_web@163.com>
2013-09-02 12:00:30 +0800

    fix dest path bug

--- tasks/template_compile.js
+++ tasks/template_compile.js
@@ -10,25 +10,44 @@
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-
   grunt.registerMultiTask('template_compile', 'Compile html template to javascript file.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
+    
+    /**
+     * 定义基本参数
+     * separator - 分隔符，只能是','
+     * baseUrl - 模版目录，来确定模版生成时对应的id
+     *
+     * eg：
+     * baseUrl = './demo/tmpl'
+     * 那么路径为'./demo/tmpl/test/index.html'的模版文件生成后对应的id将是test.index
+     */
     var options = this.options({
-      separator: ','
+      separator: ',',
+      baseUrl: ''
     });
-
+    
+    // 将模版中的单引号转义
     var process = function(src) {
       return src.replace(/\'/gm, '\\\'');
     };
 
+    // 去掉标签间的空白
     var trim = function(src) {
       return src.replace(/^\s+|\s+$/gm, '');
     };
 
-    var path2key = function(filepath){
-      return filepath.replace(/\.html?$/,'').replace(/\//g,'.');
+    // 根据文件路生成对应的id，即json对象中的key值
+    // eg：
+    // baseUrl = './demo/tmpl'
+    // './demo/tmpl/test/index.html' ==> test.index
+    var path2key = function(filepath) {
+      if (options.baseUrl) {
+        if(options.baseUrl.slice(-1) !== '/'){
+          options.baseUrl += '/';
+        }
+        filepath = filepath.replace(options.baseUrl, '');
+      }
+      return filepath.replace(/\.html?$/, '').replace(/\//g, '.');
     };
 
     // Iterate over all specified file groups.
@@ -49,6 +68,7 @@ module.exports = function(grunt) {
         return source;
       }).join(grunt.util.normalizelf(options.separator));
 
+      // 加上AMD包装
       output = 'define({' + output + '});';
 
       // Write the destination file.
